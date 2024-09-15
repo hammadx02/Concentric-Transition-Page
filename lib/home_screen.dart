be1db192438b7e4552f9bc59802aebdcfc8f913a
Hammad Ali <hammadaali02@gmail.com>
2024-09-15 16:50:35 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,13 +11,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, String>> pages = [
     {
-      'image': 'https://ouch-cdn2.icons8.com/yIxQmzj1OHH3S9AiIgelaTGdpj3wApWqSqFh80arSEA/rs:fit:784:828/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMTMx/L2NmOGIzZmY4LWRh/YjEtNDdiMS1hZGQ4/LWFkMTUwZTQwNzIx/NC5wbmc.png',
+      'image': 'https://ouch-cdn2.icons8.com/qLwkLBjIt5rjnTjksT-reemr3Rc7m8fsCWCy9VwmyKY/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjcv/MjJmNTg3NWYtNTA5/Yy00ZjhmLTg0Njgt/YjUxMjkxOWExMGI4/LnBuZw.png',
     },
     {
-      'image': 'https://ouch-cdn2.icons8.com/oEXWmNUZgj3vd_T5XSJe2dTB1GFWIBeJ1QEqRWw76O8/rs:fit:784:748/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMzM1/L2Q3MjMyYzJlLTk0/NTMtNDUzMC1iZTEx/LWQ2MDZiN2RjYmFm/Yy5wbmc.png'
+      'image': 'https://ouch-cdn2.icons8.com/aOecyQToTzM0aHNXxIeeu5hIHzPK6Nf6xpXAdEJtEgY/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjA2/LzBhNzFhMzg1LTQ5/YmYtNDUyNS05OWNm/LTNkOWQ1MmYxYWZh/NC5wbmc.png'
     },
     {
-      'image': 'https://ouch-cdn2.icons8.com/g8bZd-d4qiYuomOBWh3x61II6HlK8Ir2zp_a2cFTcCA/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNDI4/LzdjMjEyYjgzLTg0/NjQtNDQzMS04NzAz/LTNkNzk1YmUyMzli/MC5wbmc.png'
+      'image': 'https://ouch-cdn2.icons8.com/df0oDP28LtBPZxjK2SeUi36rz3yCwv9yVfB6S-OFT5M/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTE0/LzgzNjk5MTdkLWE0/N2ItNGE2NC05OGI2/LTdlMmRkYjNjMTE1/OS5wbmc.png'
     }
   ];
 
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ConcentricPageView(
         radius: 30,
         verticalPosition: 0.85,
-        colors: const [Colors.white, Colors.blueAccent, Colors.pinkAccent],
+        colors: const [Colors.white, Colors.greenAccent, Colors.teal],
         itemCount: pages.length,
         itemBuilder: (index) {
           return Container(
@@ -37,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Image.network(
                   pages[index]['image']!,
                   width: 300,
+                  height: 300,
                 )
               ],
             ),
